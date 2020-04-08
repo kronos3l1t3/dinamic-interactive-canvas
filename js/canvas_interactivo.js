b22a5cba44687a3c3f7c832735b1408c7bdfcac0
kronos <kronos3l1t3@gmail.com>
2020-04-08 12:26:31 -0400

    seleccion de posicion con el puntero

--- js/canvas_interactivo.js
+++ js/canvas_interactivo.js
@@ -1,15 +1,16 @@
 var lienzo = _g('#canvas');
 var canvas = lienzo.getContext('2d');
 
-lienzo.width = 640;
-lienzo.height = 480;
+lienzo.width = 700;
+lienzo.height = 420;
 lienzo.tabIndex = 0;
 lienzo.style = "border: 1px solid black";
+var time = null;
 
 var mousex = 0, mousey = 0;
-var scaleX = null;
-var scaleY = null;
-var rect = null;
+rect = lienzo.getBoundingClientRect();
+scaleX = lienzo.width / rect.width;
+scaleY = lienzo.height / rect.height;
 
 var props = _g('#edit_canvas');
 //Arreglo de elementos
@@ -19,19 +20,23 @@ var elements_array = {};
 var elements_properties = {
     name:null,
     img:null,
-    img_pos:{
-        img_ptox:0,
-        img_ptoy:0,
+    img_ptox: 0,
+    img_ptoy: 0,
+    shapes:{
     },
-    ptox:0,
-    ptoy:0,
-    ptos:{},
     height:0,
     width:0,
     visibility:true,
     event:{},
 }
 
+//Figuras dentro del objeto
+var shape = {
+    ptos:{
+        x:null,
+        y:null
+    }
+}
 
 
 // ++++Crud de objetos JSON++++
@@ -59,25 +64,28 @@ function edit_element(){
     var object = elements_array[select.selectedIndex];
     var properties = _g("#properties");
     var html = "<div><table>" +
-        "<tr><td><label for='name'>Nombre: </label></td><td><input id = 'name' name = 'name' type = 'text' value = '"+object.name+"'/></td></tr>" +
-        "<tr><td><label for='img'>Imagen: </label></td><td><input id = 'img' name = 'img' type='file' value = '"+object.img+"' style='color: transparent' /></td></tr>" +
-        "<tr><td><label for='img_ptox'>Posicion x: </label></td><td><input id = 'img_ptox' name = 'img_ptox' type='text' value = '"+object.ptox+"' /></td></tr>" +
-        "<tr><td><label for='img_ptoy'>Posicion y: </label></td><td><input id = 'img_ptoy' name = 'img_ptoy' type='text' value = '"+object.ptox+"' /></td></tr>" +
-        "<tr><td colspan='2'><label>Centro de la imagen: </label></td></tr>" +
-        "<tr><td><label for='ptox'>PtoX: </label></td><td><input id = 'ptox' name = 'ptox' type='text' value = '"+object.ptox+"' /></td></tr>" +
-        "<tr><td><label for='ptoy'>PtoY: </label></td><td><input id = 'ptoy' name = 'ptoy' type='text' value = '"+object.ptoy+"' /></td></tr>" +
-        "<tr><td><label for='ptos'>Vectores: </label></td><td><input id = 'ptos' name = 'ptos' type='text' value = '"+JSON.stringify(object.ptos)+"'/></td></tr>" +
-        "<tr><td><label for='height'>Altura: </label></td><td><input id = 'width' name = 'width' type='text' value = '"+object.height+"'/></td></tr>" +
-        "<tr><td><label for='width'>Ancho: </label></td><td><input id = 'height' name = 'height' type='text' value = '"+object.width+"'/></td></tr>" +
-        "<tr><td><label for='visibility'>Visible: </label></td><td><input id = 'visibility' name = 'visibility' type='checkbox' "+((object.visibility == true)?'checked':'')+"/></td></tr>" +
-        "<tr><td><label for='event'>Eventos: </label></td><td><input id = 'event' name = 'event' type='text' value = '"+object.event+"' /></td></tr>" +
-        "<tr align='center'>" +
-            "<td><input id = 'save' name = 'save' type='button' value='Guardar'/></td>" +
-            "<td><input id = 'show' name = 'show' type='button' value='Visualizar'/></td>" +
-        "</tr></table></div>";
+        "<tr><td><label for='name'>Nombre: </label></td>" +
+        "<td><input id = 'name' name = 'name' type = 'text' value = '"+object.name+"'/></td></tr>" +
+        "<tr><td><label for='img'>Imagen: </label></td>" +
+        "<td><input id = 'img' name = 'img' type='file' value = '"+object.img+"' style='color: transparent' /></td></tr>" +
+        "<tr><td><label for='img_ptox'>Posicion (x;y): </label></td>" +
+        "<td><input id = 'img_ptox' name = 'img_ptox' type='text' size='1%' value = '"+object.img_ptox+"' />" +
+        "<input id = 'img_ptoy' name = 'img_ptoy' type='text' size='1%' value = '"+object.img_ptoy+"' />" +
+        "<label for='check_point' >Marcar</label><input id = 'check_point' name = 'check_point' type = 'checkbox' /></td></tr>" +
+        "<tr><td><label for='ptos'>Figuras: </label></td>" +
+        "<td><input id = 'shapes' name = 'shapes' type='buuton'/></td></tr>" +
+        "<tr><td><label for='height'>Altura: </label></td>" +
+        "<td><input id = 'width' name = 'width' type='text' value = '"+object.width+"'/></td></tr>" +
+        "<tr><td><label for='width'>Ancho: </label></td>" +
+        "<td><input id = 'height' name = 'height' type='text' value = '"+object.height+"'/></td></tr>" +
+        "<tr><td><label for='visibility'>Visible: </label></td>" +
+        "<td><input id = 'visibility' name = 'visibility' type='checkbox' "+((object.visibility == true)?'checked':'')+"/></td></tr>" +
+        "<tr><td><label for='event'>Eventos: </label></td>" +
+        "<td><input id = 'events' name = 'events' type='text' value = '"+object.event+"' /></td></tr>" +
+        "</table></div>";
     properties.innerHTML = html;
-    addE('#save','click',save_props);
-    addE('#show','click',show_elment);
+    save();
+    setTimeout(show_elment,25);
 };
 
 // Eliminar un elemento del arreglo
@@ -91,20 +99,19 @@ function show_elment(){
     var select = _g("#elements");
     if(elements_array[select.selectedIndex].img != null){
         var img = new Image();
+        console.log(elements_array[select.selectedIndex].img);
         img.src = elements_array[select.selectedIndex].img;
         canvas.drawImage(
             img,
-            elements_array[select.selectedIndex].img_pos.img_ptox,
-            elements_array[select.selectedIndex].img_pos.img_ptox,
-            parseInt(elements_array[select.selectedIndex].height)-10,
-            parseInt(elements_array[select.selectedIndex].width)-10
+            elements_array[select.selectedIndex].img_ptox,
+            elements_array[select.selectedIndex].img_ptox,
+            parseInt(elements_array[select.selectedIndex].height),
+            parseInt(elements_array[select.selectedIndex].width)
         );
     }else{
-        console.log("status: 400, img not found");
         //return error status 400 img not found
     }
 };
-
 
 // ----Fin de Crud de Objetos JSON----
 
@@ -112,26 +119,60 @@ function show_elment(){
 
 // Salvar configuracion de los elementos
 
-function save_props(){
+function save_props(id_name){
     var select = _g("#elements");
     var object = elements_array[select.selectedIndex];
-    object.name = _g("#name").value;
-    object.img = _g("#img").value.replace('C:\\fakepath\\','img/');
-    object.img_pos.img_ptox = _g("#img_ptox").value;
-    object.img_pos.img_ptoy = _g("#img_ptoy").value;
-    object.ptox = _g("#ptox").value;
-    object.ptoy = _g("#ptoy").value;
-    object.ptos = _g("#ptos").value;
-    object.height = _g("#height").value;
-    object.width = _g("#width").value;
-    object.visibility = _g("#visibility").value;
-    object.event = _g("#event").value;
+    var sentence = "object."+id_name+" = "+
+                    ((id_name=='img')?("'img/object/"+_g("#"+id_name).value.replace("C:\\fakepath\\","")+"'"):(_g('#'+id_name).value));
+    eval(sentence);
     if(select.selectedIndex == 0){
-        lienzo.width = parseInt(object.height)+5;
-        lienzo.height = parseInt(object.width)+5;
+
+        lienzo.width = parseInt(elements_array[select.selectedIndex].height);
+        lienzo.height = parseInt(elements_array[select.selectedIndex].width);
     }
+    show_elment();
+    setTimeout(show_elment,25);
 };
 
+function save(){
+    addE('#name','keyup',save_props,'name');
+    addE('#img','change',save_props,'img');
+    addE('#img_ptox','keyup',save_props,'img_ptox');
+    addE('#img_ptoy','keyup',save_props,'img_ptoy');
+    addE('#height','keyup',save_props,'height');
+    addE('#width','keyup',save_props,'width');
+    addE('#visibility','keyup',save_props,'visibility');
+}
+
+
+// +++++++++++++++ Eventos anclados al lienzo ++++++++++++++++
+
+lienzo.addEventListener('mousemove', function (evt) {
+    rect = lienzo.getBoundingClientRect();
+    mousex = (evt.clientX - rect.left)*scaleX;
+    mousey = (evt.clientY - rect.top)*scaleY;
+}, false);
+
+lienzo.addEventListener('click', function (evt) {
+    //console.log(mousex+":"+mousey);
+    var select = _g("#elements");
+    var check_point = _g("#check_point");
+    var object = elements_array[select.selectedIndex];
+    if(check_point.checked == true){
+        _g("#img_ptox").value = Math.round(mousex,2);
+        _g("#img_ptoy").value = Math.round(mousey,2);
+        save_props("img_ptox");
+        save_props("img_ptoy");
+    }
+}, false);
+
+window.addEventListener("resize", function(){
+    rect = lienzo.getBoundingClientRect();
+    scaleX = lienzo.width / rect.width;
+    scaleY = lienzo.height / rect.height;
+}, false);
+
+// ----------------- FIN Eventos anclados al lienzo -----------------
 
 //Contar elementos de objetos
 function elements_count(){
@@ -152,12 +193,17 @@ function _g(object){
     }else if(object.indexOf('.') != -1){ // Return elements by Class
         return document.getElementsByClassName(object.replace('.',''));
     }
+
 };
 
 //Adicionar un evento a un boton
-function addE(id, event, fun){
+function addE(id, event, fun, params=null){
     _g(id).addEventListener(event,function(){
-        fun();
+        if(params != null){
+            fun(params);
+        }else{
+            fun();
+        }
     });
 };
 
@@ -175,11 +221,15 @@ function selector(){
     edit_element();
 }
 
+function generate(){
+
+}
+
 async function add_event_handler(){
     addE('#create','click',create_element);
-    addE('#edit','click',edit_element);
     addE('#remove','click',delete_element);
     addE('#elements','change',selector);
+    addE('#generate','click',generate);
 }
 
 (async function(){
@@ -187,15 +237,14 @@ async function add_event_handler(){
     props.innerHTML = "<div>" +
         "<select id='elements'><option id = 0>canvas</option></select>" +
         "<input value='Crear' id = 'create' type='button'/>" +
-        "<input value='Editar' id = 'edit' type='button'/>" +
-        "<input value='Remover' id = 'remove' type='button'/></div><br>" +
+        "<input value='Remover' id = 'remove' type='button'/>" +
+        "<input value='Generar codigo' id = 'generate' type='button'/></div><br>" +
         "<div id = 'properties'></div>";
 
     let object = clone(elements_properties);
     object.name = 'canvas';
-    object.width = 640;
-    object.height = 480;
-
+    object.height = 700;
+    object.width = 420;
     elements_array [0] = object;
     add_event_handler();
     edit_element();
